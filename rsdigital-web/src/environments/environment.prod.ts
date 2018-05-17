b7353bfc09d9e75193a3cb744f51f14709128ad0
Omar Plata <oommaarr@misena.edu.co>
2018-05-16 20:18:20 -0500

    Cambios

--- rsdigital-web/src/environments/environment.prod.ts
+++ rsdigital-web/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD1tE_0uJi2FedswoBqU1Gpu2QATzVV0PQ',
+    authDomain: 'rsdigital-2d19b.firebaseapp.com',
+    databaseURL: 'https://rsdigital-2d19b.firebaseio.com',
+    projectId: 'rsdigital-2d19b',
+    storageBucket: 'rsdigital-2d19b.appspot.com',
+    messagingSenderId: '506962553262'
+  }
 };
